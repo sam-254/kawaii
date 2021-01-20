7b864c5c744057941dd60026ae285f5b349b59b9
sam-254 <sam.danielyan25@gmail.com>
2021-01-21 03:28:24 +0400

    kawaii first commit

---
+++ kawaii/news/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, )
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = "News"
+        ordering = ['-created_at']
+        
