e69c002e3731a70c49a98a8685a8a776367af980
sam-254 <sam.danielyan25@gmail.com>
2021-01-27 10:29:43 +0400

    add category navigation sidebar

--- kawaii/news/views.py
+++ kawaii/news/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'News list'
+        'title': 'News list',
+        'categories': categories
         }
     return render(request, 'news/index.html',context=context)
+
+
+def get_category(request, category_id):
+    current_category_news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    current_category = Category.objects.get(pk=category_id)
+
+    return render(request, 'news/category.html', {
+        'current_category_news': current_category_news,
+        'categories': categories,
+        'current_category': current_category})
