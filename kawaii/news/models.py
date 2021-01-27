bfba26fc378e2eb484e4d983482ffc559b1fb91a
sam-254 <sam.danielyan25@gmail.com>
2021-01-27 19:24:01 +0400

    add pages for any news article| modify views

--- kawaii/news/models.py
+++ kawaii/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class News(models.Model):
@@ -9,6 +10,9 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def get_absolute_url(self):
+        return reverse_lazy('view_news', kwargs={"news_id": self.pk})
 
     def __str__(self):
         return self.title
@@ -21,6 +25,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Category Name')
+
+    def get_absolute_url(self):
+        return reverse_lazy('category', kwargs={"category_id": self.pk})
 
     def __str__(self):
         return self.title
--- kawaii/news/views.py
+++ kawaii/news/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
 
@@ -15,9 +14,17 @@ def index(request):
 
 def get_category(request, category_id):
     current_category_news = News.objects.filter(category_id=category_id)
-    current_category = Category.objects.get(pk=category_id)
+    # current_category = Category.objects.get(pk=category_id)
+    current_category = get_object_or_404(Category, pk=category_id)
+
 
     return render(request, 'news/category.html', {
         'current_category_news': current_category_news,
         # 'categories': categories,
         'current_category': current_category})
+
+
+def view_news(request, news_id: int):
+    # current_news = News.objects.get(pk=news_id)
+    current_news = get_object_or_404(News, pk=news_id)
+    return render(request,  'news/view_news.html', {'current_news': current_news})
